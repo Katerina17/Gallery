f9608479cba37af3152ac99e7413ca2cd82a91ab
Katerina17 <pukhovaekaterina@gmail.com>
2016-03-23 14:08:22 +0200

    Init

---
+++ script1.js
@@ -0,0 +1,17 @@
+/*function showPhoto() {
+    document.getElementById ("bigImg").style.visibility = "visible";
+}
+
+function hiddenPhoto() {
+    document.getElementById ("bigImg").style.visibility = "hidden";
+}
+*/
+
+
+/*function showImage() {
+    var hiddenGallery = document.getElementById('hiddengallery');
+    hiddenGallery.style.display = 'block';
+}
+function () {
+    document.getElementById('hiddengallery').style.display = 'none';
+}*/
